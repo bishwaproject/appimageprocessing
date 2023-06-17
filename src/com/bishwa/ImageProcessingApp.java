9fcc707fa43f61d624e0f664b00c3221c155f7ae
bishwajava <emailofbishwa@gmail.com>
2023-06-17 11:19:25 -0400

    ImageProcessingApp added with class fields

--- src/com/bishwa/ImageProcessingApp.java
+++ src/com/bishwa/ImageProcessingApp.java
@@ -1,8 +1,27 @@
 package com.bishwa;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 public class ImageProcessingApp {
-	//Client Class: 
+	// Client Class: private JFrame frame;
+	private JButton uploadButton;
+	private JButton addButton;
+	private JButton processButton;
+	private JTextArea outputTextArea;
+	private List<File> selectedImages;
+
+	public ImageProcessingApp() {
+		selectedImages = new ArrayList<>();
+
+	}
+
 	public static void main(String[] args) {
-		
+
 	}
 }
